56ac6bc5871f0b6f2edcd06ee6dde72fee19ece0
Hannah Mills <millsh56@gmail.com>
2023-07-01 19:58:36 -0400

    finished positioning

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// create a new image function
+// create new image function
 function newImage(url, left, bottom){
     let image = document.createElement('img')
     image.src = url
+    image.style.position = 'fixed'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
-
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
-
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
@@ -23,8 +20,6 @@ function newItem(url, left, bottom){
     return item
 }
 
-// positioning functions
-
 function move(image){
     image.style.position = 'fixed'
     
@@ -32,29 +27,28 @@ function move(image){
         image.style.left = left + 'px'
         image.style.bottom = bottom + 'px'
     }
+
     return {
         to: moveToCoordinates
     }
-    
 }
-
-// code positioning images
 
 move(newImage('assets/green-character.gif')).to(100, 250)
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
 
-// code positioning items
-
-
 function newInventory(){
     let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
-    inventory.style.bottom = '0px';
-    inventory.style.left = '0px'
+    // inventory.style.bottom = '0px'
+    // inventory.style.left = '0px'
     inventory.style.width = '100%'
     inventory.style.height = '100px'
     inventory.style.display = 'flex'
@@ -69,4 +63,4 @@ function newInventory(){
 
 const inventory = newInventory()
 
-
+move(inventory).to(0, 0)
